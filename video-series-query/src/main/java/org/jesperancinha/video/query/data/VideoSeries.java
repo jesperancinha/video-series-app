34dbfa0e9ebc7724e9b77c6f9b2c7d9daf3e610b
Joao Esperancinha <jofisaes@gmail.com>
2021-08-21 12:42:25 +0200

    🌿 Hashcode calculation improvement

--- video-series-query/src/main/java/org/jesperancinha/video/query/data/VideoSeries.java
+++ video-series-query/src/main/java/org/jesperancinha/video/query/data/VideoSeries.java
@@ -3,10 +3,8 @@ package org.jesperancinha.video.query.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.Hibernate;

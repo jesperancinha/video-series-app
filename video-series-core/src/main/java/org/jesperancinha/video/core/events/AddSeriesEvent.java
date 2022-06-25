6e2f950aea52fe45569048f03a6a9024c1d33c78
Joao Esperancinha <jofisaes@gmail.com>
2022-06-25 13:11:33 +0200

    Testing @Getter @Setter Lombok

--- video-series-core/src/main/java/org/jesperancinha/video/core/events/AddSeriesEvent.java
+++ video-series-core/src/main/java/org/jesperancinha/video/core/events/AddSeriesEvent.java
@@ -1,6 +1,8 @@
 package org.jesperancinha.video.core.events;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.jesperancinha.video.core.data.Genre;
 
 import java.math.BigDecimal;
@@ -9,50 +11,12 @@ import java.math.BigDecimal;
  * Unfortunately, serialization of the events is not possible using records and lombok as of now.
  */
 @Builder
+@Getter
+@Setter
 public class AddSeriesEvent {
     String id;
     String name;
     Integer volumes;
     BigDecimal cashValue;
     Genre genre;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getVolumes() {
-        return volumes;
-    }
-
-    public void setVolumes(Integer volumes) {
-        this.volumes = volumes;
-    }
-
-    public BigDecimal getCashValue() {
-        return cashValue;
-    }
-
-    public void setCashValue(BigDecimal cashValue) {
-        this.cashValue = cashValue;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
 }

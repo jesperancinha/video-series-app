128751e10e29f2600a38dca36270d78442dec773
Joao Esperancinha <jofisaes@gmail.com>
2022-07-11 23:19:36 +0200

    VSA Open API

--- video-series-command/src/main/java/org/jesperancinha/video/command/VideoAppCommandLauncher.java
+++ video-series-command/src/main/java/org/jesperancinha/video/command/VideoAppCommandLauncher.java
@@ -1,5 +1,8 @@
 package org.jesperancinha.video.command;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.jesperancinha.video.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +10,11 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(AxonConfig.class)
+@OpenAPIDefinition(
+        info = @Info(title = "OpenAPI definition"),
+        servers = @Server(url = "${vsa.server.url}",
+                description = "Server URL")
+)
 public class VideoAppCommandLauncher {
     public static void main(String[] args) {
         SpringApplication.run(VideoAppCommandLauncher.class);

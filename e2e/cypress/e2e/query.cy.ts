358ebb090eebc62ad5b91c5af2035af9b9da611d
Joao Esperancinha <jofisaes@gmail.com>
2022-07-11 22:16:12 +0200

    Configures dynamic hosts

--- e2e/cypress/e2e/query.cy.ts
+++ e2e/cypress/e2e/query.cy.ts
@@ -1,7 +1,7 @@
 describe('empty spec', () => {
     it('should perform a GET request to Query and receive QAF', () => {
         let host = Cypress.env('host.query') ? Cypress.env('host.query') : 'localhost';
-        cy.request("GET", `http://${host.command}:8090/video-series`).its("body").should(body => {
+        cy.request("GET", `http://${host}:8090/video-series`).its("body").should(body => {
             const expected = {
                 "name": "Glee",
                 "volumes": 121,

b31dc56672c66a2fd533199921f305316e2a30c8
Joao Esperancinha <jofisaes@gmail.com>
2022-08-06 11:02:52 +0200

    Video and Screen generation not needed for cypress

--- e2e/full.cypress.config.ts
+++ e2e/full.cypress.config.ts
@@ -5,7 +5,5 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    video: false,
-    screenshotOnRunFailure: false
   },
 });

05b4e3279d4fe2d892b6f1f4194867e549ae6127
Joao Esperancinha <jofisaes@gmail.com>
2022-09-16 00:16:50 +0200

    Ignores x-stream versions 1.4.18 to 1.4.19

--- video-series-core/src/main/java/org/jesperancinha/video/core/configuration/AxonConfig.java
+++ video-series-core/src/main/java/org/jesperancinha/video/core/configuration/AxonConfig.java
@@ -4,6 +4,10 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.security.NullPermission;
+import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
@@ -11,10 +15,12 @@ import org.axonframework.extensions.mongo.MongoTemplate;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
 import org.axonframework.serialization.Serializer;
+import org.jesperancinha.video.core.events.AddSeriesEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collection;
 import java.util.List;
 
 @Configuration
@@ -30,7 +36,12 @@ public class AxonConfig {
     private String mongoDatabase;
 
     @Bean
-    public TokenStore tokenStore(Serializer serializer) {
+    public TokenStore tokenStore(Serializer serializer, XStream xstream) {
+        xstream.addPermission(NoTypePermission.NONE);
+        xstream.addPermission(NullPermission.NULL);
+        xstream.addPermission(PrimitiveTypePermission.PRIMITIVES);
+        xstream.allowTypes(new Class[]{AddSeriesEvent.class});
+        xstream.allowTypeHierarchy(Collection.class);
         return MongoTokenStore.builder().mongoTemplate(axonMongoTemplate()).serializer(serializer).build();
     }
 
--- video-series-query/src/main/java/org/jesperancinha/video/query/VideoAppQueryLauncher.java
+++ video-series-query/src/main/java/org/jesperancinha/video/query/VideoAppQueryLauncher.java
@@ -1,18 +1,13 @@
 package org.jesperancinha.video.query;
 
+import com.thoughtworks.xstream.XStream;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.jesperancinha.video.core.configuration.AxonConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 @SpringBootApplication
 @Import(AxonConfig.class)
@@ -22,7 +17,9 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
                 description = "Server URL")
 )
 public class VideoAppQueryLauncher {
+
     public static void main(String[] args) {
         SpringApplication.run(VideoAppQueryLauncher.class);
     }
+
 }

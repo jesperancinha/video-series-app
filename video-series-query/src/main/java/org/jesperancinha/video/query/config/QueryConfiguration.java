194b455904bda353a6e0e15fc8a0a933f5b97306
Joao Esperancinha <jofisaes@gmail.com>
2022-12-06 19:56:22 +0100

    Creates XTream seraializers

---
+++ video-series-query/src/main/java/org/jesperancinha/video/query/config/QueryConfiguration.java
@@ -0,0 +1,35 @@
+package org.jesperancinha.video.query.config;
+
+import com.thoughtworks.xstream.XStream;
+import org.axonframework.commandhandling.AsynchronousCommandBus;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
+import org.axonframework.config.DefaultConfigurer;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryConfiguration {
+
+    @Bean
+    public XStream xStream() {
+        XStream xStream = new XStream();
+        xStream.allowTypesByWildcard(new String[]{
+                "org.axonframework.**",
+                "org.jesperancinha.**"
+        });
+        return new XStream();
+    }
+
+    @Bean
+    public Serializer serializer(XStream xStream) {
+        XStreamSerializer xStreamSerializer = XStreamSerializer.builder().xStream(xStream).build();
+        DefaultConfigurer.defaultConfiguration()
+                .configureSerializer(configuration -> xStreamSerializer)
+                .configureMessageSerializer(configuration -> xStreamSerializer)
+                .configureEventSerializer(configuration -> xStreamSerializer);
+        return xStreamSerializer;
+    }
+}

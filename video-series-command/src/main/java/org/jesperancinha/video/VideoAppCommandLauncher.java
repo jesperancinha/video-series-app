1af4eade6c79a178889a233a14c2d457d991a27f
Joao Esperancinha <jofisaes@gmail.com>
2022-12-09 14:43:48 +0100

    Testing with enable axon

--- video-series-command/src/main/java/org/jesperancinha/video/VideoAppCommandLauncher.java
+++ video-series-command/src/main/java/org/jesperancinha/video/VideoAppCommandLauncher.java
@@ -4,8 +4,7 @@ package org.jesperancinha.video;
 //import io.swagger.v3.oas.annotations.info.Info;
 //import io.swagger.v3.oas.annotations.servers.Server;
 
-import org.axonframework.springboot.autoconfig.EventProcessingAutoConfiguration;
-import org.axonframework.springboot.autoconfig.InfraConfiguration;
+import org.axonframework.spring.config.EnableAxon;
 import org.jesperancinha.video.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +16,7 @@ import org.springframework.context.annotation.Import;
 //        servers = @Server(url = "${vsa.server.url}",
 //                description = "Server URL")
 //)
+@EnableAxon
 public class VideoAppCommandLauncher {
     public static void main(String[] args) {
         SpringApplication.run(VideoAppCommandLauncher.class);
--- video-series-command/src/main/java/org/jesperancinha/video/command/config/CommandConfiguration.java
+++ video-series-command/src/main/java/org/jesperancinha/video/command/config/CommandConfiguration.java
@@ -3,19 +3,25 @@ package org.jesperancinha.video.command.config;
 import org.axonframework.commandhandling.AsynchronousCommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
+import org.axonframework.config.Configurer;
+import org.axonframework.config.DefaultConfigurer;
+import org.jesperancinha.video.command.aggregates.VideoSeriesAggregate;
+import org.jesperancinha.video.command.commands.AddVideoSeriesCommand;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommandConfiguration {
-
-    @Bean
-    public CommandGateway commandGateway() {
-        return DefaultCommandGateway.builder()
-                .commandBus(AsynchronousCommandBus
-                        .builder()
-                        .build())
-                .build();
-    }
+//
+//    @Bean
+//    public CommandGateway commandGateway() {
+//        Configurer configurer = DefaultConfigurer.defaultConfiguration();
+//        configurer.registerCommandHandler(c -> new VideoSeriesAggregate(c.getComponent(AddVideoSeriesCommand.class)));
+//        return DefaultCommandGateway.builder()
+//                .commandBus(AsynchronousCommandBus
+//                        .builder()
+//                        .build())
+//                .build();
+//    }
 
 }

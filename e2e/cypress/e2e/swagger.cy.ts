825cf304cd4761153f0b9ab93eba51590c837b2e
Joao Esperancinha <jofisaes@gmail.com>
2022-12-11 20:16:24 +0100

    Swagger

--- e2e/cypress/e2e/swagger.cy.ts
+++ e2e/cypress/e2e/swagger.cy.ts
@@ -1,21 +1,21 @@
 describe('Favorite Lyrics E2E Tests', () => {
   it('shows swagger for Command', () => {
     cy.log("Test Disabled");
-    // let host = Cypress.env('host.command') ?  Cypress.env('host.command') : 'localhost';
-    // cy.visit(`http://${host}:8080/swagger-ui/index.html`);
-    // cy.get('h2').contains('OpenAPI definition').should('not.be.null');
-    // cy.wait(1000);
-    //
-    // cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8080')
+    let host = Cypress.env('host.command') ?  Cypress.env('host.command') : 'localhost';
+    cy.visit(`http://${host}:8080/swagger-ui/index.html`);
+    cy.get('h2').contains('OpenAPI definition').should('not.be.null');
+    cy.wait(1000);
+
+    cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8080')
   });
 
   it('shows swagger for Query', () => {
     cy.log("Test Disabled");
-    // let host = Cypress.env('host.query') ?  Cypress.env('host.query') : 'localhost';
-    // cy.visit(`http://${host}:8090/swagger-ui/index.html`);
-    // cy.get('h2').contains('OpenAPI definition').should('not.be.null');
-    // cy.wait(1000);
-    //
-    // cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8090')
+    let host = Cypress.env('host.query') ?  Cypress.env('host.query') : 'localhost';
+    cy.visit(`http://${host}:8090/swagger-ui/index.html`);
+    cy.get('h2').contains('OpenAPI definition').should('not.be.null');
+    cy.wait(1000);
+
+    cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:8090')
   });
 })

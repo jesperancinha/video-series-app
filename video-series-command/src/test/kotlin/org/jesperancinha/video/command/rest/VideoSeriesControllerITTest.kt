2e12d21017c948ddf6df7d167e6876850aa6cb49
Joao Esperancinha <jofisaes@gmail.com>
2022-12-11 09:00:25 +0100

    Splits docker compose from docker compose db

--- video-series-command/src/test/kotlin/org/jesperancinha/video/command/rest/VideoSeriesControllerITTest.kt
+++ video-series-command/src/test/kotlin/org/jesperancinha/video/command/rest/VideoSeriesControllerITTest.kt
@@ -1,23 +1,33 @@
 package org.jesperancinha.video.command.rest
 
-import com.jayway.jsonpath.Criteria
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.core.test.TestCase
 import io.kotest.extensions.spring.SpringExtension
 import org.jesperancinha.video.core.data.Genre
 import org.jesperancinha.video.core.data.VideoSeriesDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.web.client.postForEntity
+import org.testcontainers.containers.DockerComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait.forListeningPort
 import org.testcontainers.junit.jupiter.Testcontainers
+import java.io.File
 import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Testcontainers
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@ContextConfiguration(initializers = [VideoSeriesControllerITTest.VideoSeriesCommandInitializer::class])
 class VideoSeriesControllerITTest(
     @Autowired
     private val jdbcTemplate: JdbcTemplate,
@@ -82,5 +92,29 @@ class VideoSeriesControllerITTest(
 //        mongoDBContainer.isRunning.shouldBeTrue()
 
     }
+
+    class VideoSeriesCommandInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        private val dockerCompose by lazy {
+            DockerCompose(listOf(File("../docker-compose-db.yml")))
+                .withExposedService("postgres_1", 5432, forListeningPort())
+                .withExposedService("postgres-es_1", 5432, forListeningPort())
+                .withLocalCompose(true)
+                .also { it.start() }
+        }
+
+        override fun initialize(applicationContextx: ConfigurableApplicationContext) {
+            logger.info("Starting IT -> ${LocalDateTime.now()}")
+            logger.info("Starting service 1 at ${dockerCompose.getServiceHost("postgres_1", 5432)}")
+            logger.info("Starting service 1 at ${dockerCompose.getServiceHost("postgres-es_1", 5432)}")
+            logger.info("End IT -> ${LocalDateTime.now()}")
+            logger.info("Time Elapsed IT -> ${ChronoUnit.MILLIS.between(startup, LocalDateTime.now())} ms")
+        }
+
+        companion object {
+            val logger: Logger = LoggerFactory.getLogger(VideoSeriesControllerITTest::class.java)
+            private val startup: LocalDateTime = LocalDateTime.now()
+        }
+    }
+    private class DockerCompose(files: List<File>) : DockerComposeContainer<DockerCompose>(files)
 
 }

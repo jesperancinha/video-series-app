ea8106eced0024fbed17cf28f10ed168e8407d68
Joao Esperancinha <jofisaes@gmail.com>
2022-12-13 20:24:38 +0100

    XStream again

--- video-series-mono/src/main/java/org/jesperancinha/video/config/AxonConfig.java
+++ video-series-mono/src/main/java/org/jesperancinha/video/config/AxonConfig.java
@@ -1,37 +1,60 @@
 package org.jesperancinha.video.config;
 
+import com.mongodb.MongoClient;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.security.NullPermission;
+import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
 import org.axonframework.spring.config.AxonConfiguration;
+import org.jesperancinha.video.event.VideoSeriesEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mongodb.MongoClient;
+import java.util.Collection;
 
 @Configuration
 public class AxonConfig {
-	@Value("${video.series.mongo.port}")
-	private Long mongoPort;
+    @Value("${video.series.mongo.port}")
+    private Long mongoPort;
 
-	@Bean
-	public MongoClient mongoClient(){
-		return new MongoClient("localhost", mongoPort.intValue());
-	}
-	
-	@Bean
-	public EventStorageEngine storageEngine(MongoClient client) {
-	    return MongoEventStorageEngine.builder().mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build()).build();
-	}
+    @Bean
+    public Serializer serializer() {
+        XStream xStream = new XStream();
+        xStream.allowTypesByWildcard(new String[]{
+                "org.axonframework.**",
+                "org.jesperancinha.**"
+        });
+        xStream.addPermission(NoTypePermission.NONE);
+        xStream.addPermission(NullPermission.NULL);
+        xStream.addPermission(PrimitiveTypePermission.PRIMITIVES);
+        xStream.allowTypes(new Class[]{VideoSeriesEvent.class});
+        xStream.allowTypeHierarchy(Collection.class);
+        return XStreamSerializer.builder().xStream(xStream).build();
+    }
 
-	@Bean
-	public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration configuration) {
-	    return EmbeddedEventStore.builder()
-	            .storageEngine(storageEngine)
-	            .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
-	            .build();
-	}
+    @Bean
+    public MongoClient mongoClient() {
+        return new MongoClient("localhost", mongoPort.intValue());
+    }
+
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient client) {
+        return MongoEventStorageEngine.builder().mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build()).build();
+    }
+
+    @Bean
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration configuration) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
+                .build();
+    }
 }

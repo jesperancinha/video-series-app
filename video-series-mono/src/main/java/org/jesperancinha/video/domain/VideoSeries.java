99ffdb7b23bf2bf1ce9c25ea189f593e83f66af7
Joao Esperancinha <jofisaes@gmail.com>
2022-12-15 23:55:20 +0100

    Javax to Jakarta

--- video-series-mono/src/main/java/org/jesperancinha/video/domain/VideoSeries.java
+++ video-series-mono/src/main/java/org/jesperancinha/video/domain/VideoSeries.java
@@ -1,7 +1,7 @@
 package org.jesperancinha.video.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;

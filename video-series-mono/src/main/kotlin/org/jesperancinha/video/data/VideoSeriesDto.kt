f0516ecfa908b3655648047662dfdaf8c2cc37ea
Joao Esperancinha <j.esperancinha@coin.nl>
2023-01-06 18:44:02 +0100

    Serializing for jackson

--- video-series-mono/src/main/kotlin/org/jesperancinha/video/data/VideoSeriesDto.kt
+++ video-series-mono/src/main/kotlin/org/jesperancinha/video/data/VideoSeriesDto.kt
@@ -1,11 +1,17 @@
 package org.jesperancinha.video.data
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.math.BigDecimal
 
 data class VideoSeriesDto (
+    @JsonProperty("id")
     val id: Long,
+    @JsonProperty("name")
     val name: String,
+    @JsonProperty("volumes")
     val volumes: Int = 1,
+    @JsonProperty("cashValue")
     val cashValue: BigDecimal,
+    @JsonProperty("genre")
     val genre: Genre
 )

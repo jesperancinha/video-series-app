6e1246319140060c8c9edd053f5660a3d7de398f
Joao Esperancinha <j.esperancinha@coin.nl>
2023-01-06 18:33:23 +0100

    Updates Dto

--- video-series-mono/src/main/kotlin/org/jesperancinha/video/data/VideoSeriesDto.kt
+++ video-series-mono/src/main/kotlin/org/jesperancinha/video/data/VideoSeriesDto.kt
@@ -2,7 +2,7 @@ package org.jesperancinha.video.data
 
 import java.math.BigDecimal
 
-data class VideoSeriesDto constructor(
+data class VideoSeriesDto (
     val id: Long,
     val name: String,
     val volumes: Int = 1,

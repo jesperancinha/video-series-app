e0c9ca2aba5f9c685f8f675cb14fe0ca1ea1af2a
João Esperancinha <jofisaes@gmail.com>
2025-11-09 15:36:00 +0100

    Enable videos and failure screenshots in Cypress tests, switch to `module.exports` in full Cypress config, adjust `Dockerfile` ENV formatting, and refine Cypress selectors for swagger tests.

--- e2e/full.cypress.config.ts
+++ e2e/full.cypress.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "cypress";
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    screenshotOnRunFailure: true,
     supportFile: `${__dirname}/cypress/support/e2e.ts`,
     specPattern: `${__dirname}/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}`,
     retries: {
